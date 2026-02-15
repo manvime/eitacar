a94dbc0b63312d4a8d9356a8d39811844f8fc0b3
manvime <94940614+manvime@users.noreply.github.com>
2026-02-15 12:49:56 -0300

    Update app

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,7 @@ export const metadata = {
   },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
       <body style={{ margin: 0, background: "#000", color: "#fff" }}>
